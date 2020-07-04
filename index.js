3ad9a3d14f5b7d1d028b863997a5c44a4f8bd824
Anton Bman <antonidag@hotmail.com>
2020-07-04 13:53:19 +0200

    Clean up in code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const request = require('request');
 const fs = require('fs');
-const dir = 'C:/Users/anbj/Documents/CustomerJobbs/tmp';
+const { Console } = require('console');
+const dir = 'C:/Users/anton/Documents/NewsletterArchive';
 
 
 if (!fs.existsSync(dir)){
@@ -17,33 +18,54 @@ const options = {
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Basic64encodedAPIkey',
+        'Authorization': 'Basic <64BaseEncodedAPIKEY>',
         'Accept-Encoding': 'gzip,deflate'
     }
 };
 
-request(options,(err, res, body) => {
-  if (err) { return console.log(err); }
-  
-  let json = JSON.parse(body);
-  console.log("Getting Poll..")
-  console.log(json.Result.PollURL);
 
 
-  
-  request(json.Result.PollURL,(err, res, body) => {
-    if (err) { return console.log(err); }
-    
-    let json = JSON.parse(body);
-    console.log("Getting Dataurl..")
-    console.log(body);
-    console.log(json.DataUrl);
-    
-    request(json.DataUrl,(err, res, body) => {
-        if (err) { return console.log(err); }
+
+async function GetPollUrl(){
+  console.log('Getting Poll url..')
+  return new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+        if (error) reject(error);
+        if (response.statusCode != 200) {
+            reject('Invalid status code <' + response.statusCode + '>');
+        }
         let json = JSON.parse(body);
+        resolve(json.Result.PollURL);
+    });
+  });
+}
+
+
+async function GetDataUrl(url){
+  console.log('Getting Data url..')
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+        if (error) reject(error);
+        if (response.statusCode != 200) {
+            reject('Invalid status code <' + response.statusCode + '>');
+        }
+        let json = JSON.parse(body);
+        resolve(json);
+    });
+  });
+}
+
+async function GetData(url){
+  console.log('Getting Data url..')
+
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+        if (error) reject(error);
+        if (response.statusCode != 200) {
+            reject('Invalid status code <' + response.statusCode + '>');
+        }
         console.log("All Newsletters...");
-        
+        let json = JSON.parse(body);
 
         json.forEach(function (item, index) {
             var str = item.Name;
@@ -58,14 +80,40 @@ request(options,(err, res, body) => {
           });
         });
     });
-});
+}
+
+async function main(){
+  const pollrul = await GetPollUrl();
+  console.log(pollrul);
+  
+  let run = true;
+  let dataurl = '';
+  while(run){
+    const json = await GetDataUrl(pollrul);
+    if(json.StateName == 'Completed'){
+      console.log.log("Data ready...")
+      run = false;
+      dataurl = json.DataUrl;
+    }
+    if(json.StateName != 'Completed'){
+      await sleep(5000);
+      console.log("...polling for data...")
+    }
+  }
+  const data = await GetData(dataurl);
+
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+} 
+
+main();
 
 
-function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  } 
+
 
 
   
